4dccd778ab7548ab5f96aa9f48af1da5ecfd87a3
Gea-Suan Lin <gslin@gslin.org>
2019-05-06 13:29:11 +0800

    Bugfix.

--- quick-open-tabs-in-alabout.user.js
+++ quick-open-tabs-in-alabout.user.js
@@ -2,16 +2,16 @@
 // @name        Quick Open Tabs in Alabout
 // @namespace   https://wiki.gslin.org/wiki/quick-open-tabs-alabout
 // @include     http://alabout.com/*
-// @version     0.20190510.0
+// @version     0.20190506.0
 // @grant       GM_openInTab
 // @license     MIT
 // ==/UserScript==
 
-(function(){
+(() => {
     for (let el of document.querySelectorAll('table[id^="ml_"] tr:first-child .mn')) {
         let tr = document.createElement('tr');
         tr.innerHTML = '<td></td><td><input type="button" style="font-size: 2em;" value="Quick Open Tabs"/></td>';
-        tr.querySelector('input').addEventListener('click', () => {
+        tr.querySelector('input').addEventListener('click', function(){
             let t = this.parentElement.parentElement.parentElement.getElementsByTagName('tr')[0];
             for (let link of t.querySelectorAll('a')) {
                 GM_openInTab(link.href, true);
