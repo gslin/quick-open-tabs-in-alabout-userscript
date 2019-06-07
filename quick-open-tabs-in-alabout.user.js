653d92bd0339537614325d28f49c325768d994c3
Gea-Suan Lin <gslin@gslin.org>
2019-06-07 15:24:41 +0800

    Add 'use strict'; as best practice...

--- quick-open-tabs-in-alabout.user.js
+++ quick-open-tabs-in-alabout.user.js
@@ -2,12 +2,14 @@
 // @name        Quick Open Tabs in Alabout
 // @namespace   https://wiki.gslin.org/wiki/quick-open-tabs-alabout
 // @include     http://alabout.com/*
-// @version     0.20190506.0
+// @version     0.20190607.0
 // @grant       GM_openInTab
 // @license     MIT
 // ==/UserScript==
 
 (() => {
+    'use strict';
+
     for (let el of document.querySelectorAll('table[id^="ml_"] tr:first-child .mn')) {
         let tr = document.createElement('tr');
         tr.innerHTML = '<td></td><td><input type="button" style="font-size: 2em;" value="Quick Open Tabs"/></td>';
