699d0ce1cd9e1d395fd7fa6f5209720e6191022e
Gea-Suan Lin <gslin@gslin.org>
2020-02-09 22:14:37 +0800

    Add tabindex.

--- quick-open-tabs-in-alabout.user.js
+++ quick-open-tabs-in-alabout.user.js
@@ -2,7 +2,7 @@
 // @name        Quick Open Tabs in Alabout
 // @namespace   https://wiki.gslin.org/wiki/quick-open-tabs-alabout
 // @include     http://alabout.com/*
-// @version     0.20190607.0
+// @version     0.20200209.0
 // @grant       GM_openInTab
 // @license     MIT
 // ==/UserScript==
@@ -12,7 +12,7 @@
 
     for (let el of document.querySelectorAll('table[id^="ml_"] tr:first-child .mn')) {
         let tr = document.createElement('tr');
-        tr.innerHTML = '<td></td><td><input type="button" style="font-size: 2em;" value="Quick Open Tabs"/></td>';
+        tr.innerHTML = '<td></td><td><input type="button" style="font-size: 2em;" tabindex="1" value="Quick Open Tabs"/></td>';
         tr.querySelector('input').addEventListener('click', function(){
             let t = this.parentElement.parentElement.parentElement.getElementsByTagName('tr')[0];
             for (let link of t.querySelectorAll('a')) {
